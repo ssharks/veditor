fc697a38dac19d7b75455e20af3192e21225d5af
tadashi-k <tadashi-k@3f283593-323e-0410-93fe-8b41104a9e7c>
2006-05-14 03:44:16 +0000

    modify VHDL keyword
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@124 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/editor/HdlScanner.java
+++ src/src/net/sourceforge/veditor/editor/HdlScanner.java
@@ -69,14 +69,16 @@ public class HdlScanner extends RuleBasedScanner
 			"attribute", "begin", "block", "body", "buffer", "bus", "case",
 			"component", "configuration", "constant", "disconnect", "downto",
 			"else", "elsif", "end", "entity", "exit", "file", "for",
-			"function", "generate", "generic", "guarded", "if", "in", "inout",
-			"is", "label", "library", "linkage", "loop", "map", "mod", "nand",
-			"new", "next", "nor", "not", "null", "of", "on", "open", "or",
-			"others", "out", "package", "port", "procedure", "process", "range",
-			"record", "register", "rem", "report", "return", "select",
-			"severity", "signal", "subtype", "then", "to", "transport", "type",
-			"units", "until", "use", "variable ", "wait", "when", "while",
-			"with", "xor" };
+			"function", "generate", "generic", "guarded", "if", "impure", "in",
+			"inertial", "inout", "is", "label", "library", "linkage",
+			"literal", "loop", "map", "mod", "nand", "new", "next", "nor",
+			"not", "null", "of", "on", "open", "or", "others", "out",
+			"package", "port", "postponed", "procedure", "process", "pure",
+			"range", "record", "register", "reject", "rem", "report", "return",
+			"rol", "ror", "select", "severity", "shared", "signal", "sla",
+			"sll", "sra", "srl", "subtype", "then", "to", "transport", "type",
+			"unaffected", "units", "until", "use", "variable", "wait", "when",
+			"while", "with", "xnor", "xor" };
 
 	private HdlScanner(ColorManager manager, boolean isVerilog)
 	{
