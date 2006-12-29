fc0b8283fb60e335b977ad3a43f2d1ebcf46f2b0
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2006-12-29 00:11:56 +0000

    Added Template Store and Context
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@161 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/VerilogPlugin.java
+++ src/src/net/sourceforge/veditor/VerilogPlugin.java
@@ -11,23 +11,34 @@
 
 package net.sourceforge.veditor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import net.sourceforge.veditor.template.VerilogContextType;
+import net.sourceforge.veditor.template.VhdlInModuleContextType;
+import net.sourceforge.veditor.template.VhdlOutModuleContextType;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceConverter;
+import org.eclipse.jface.text.templates.ContextTypeRegistry;
+import org.eclipse.jface.text.templates.persistence.TemplateStore;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.ui.editors.text.templates.ContributionContextTypeRegistry;
+import org.eclipse.ui.editors.text.templates.ContributionTemplateStore;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
@@ -38,11 +49,17 @@ public class VerilogPlugin extends AbstractUIPlugin
 	private static final String CONSOLE_NAME = "veditor";
 	private static VerilogPlugin plugin;
 	private static final String MARKER_TYPE = "org.eclipse.core.resources.problemmarker";
+	private static final String CUSTTOM_TEMPLATES_PREFERENCE_NAME = "net.sourceforge.veditor.templatesStore";	
+	protected TemplateStore templateStore;
+	protected ContributionContextTypeRegistry contextTypeRegistry;
+	public static final String ID="net.sourceforge.veditor";
 
 	public VerilogPlugin()
 	{
 		super();
-		plugin = this;
+		plugin = this;		
+		templateStore=null;
+		contextTypeRegistry=null;		
 	}
 
 	/**
@@ -250,6 +267,39 @@ public class VerilogPlugin extends AbstractUIPlugin
 		catch (CoreException e)
 		{
 		}
+	}
+	
+	/**
+	 * Returns this plug-in's template store.
+	 * 
+	 * @return the template store of this plug-in instance
+	 */
+	public TemplateStore getTemplateStore() {
+		if (templateStore == null) {
+			templateStore= new ContributionTemplateStore(getContextTypeRegistry(), getPlugin().getPreferenceStore(), CUSTTOM_TEMPLATES_PREFERENCE_NAME);
+			try {
+				templateStore.load();
+			} catch (IOException e) {
+				getPlugin().getLog().log(new Status(IStatus.ERROR, "net.sourceforge.veditor", IStatus.OK, "", e)); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+		}
+		return templateStore;
+	}
+
+	/**
+	 * Returns this plug-in's context type registry.
+	 * 
+	 * @return the context type registry for this plug-in instance
+	 */
+	public ContextTypeRegistry getContextTypeRegistry() {
+		if (contextTypeRegistry == null) {
+			// create an configure the contexts available in the template editor
+			contextTypeRegistry= new ContributionContextTypeRegistry();
+			contextTypeRegistry.addContextType(VhdlInModuleContextType.CONTEXT_TYPE);			
+			contextTypeRegistry.addContextType(VhdlOutModuleContextType.CONTEXT_TYPE);
+			contextTypeRegistry.addContextType(VerilogContextType.CONTEXT_TYPE);
+		}
+		return contextTypeRegistry;
 	}
 }
 
