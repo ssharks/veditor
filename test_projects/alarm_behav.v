0c333e9fa0a0a59ac9961ef6e7618789e105de0c
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-03-18 14:50:20 +0000

    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@526 3f283593-323e-0410-93fe-8b41104a9e7c

--- test_projects/alarm_behav.v
+++
@@ -1,8 +0,0 @@
-/* behavioral implementation of alarm module */
-module alarm_behav(alrm, day2, day1, day0);
-
-	input day2, day1, day0;
-	output alrm;
-
-	assign alrm = ~day2 & ~day0  |  ~day1 & ~day0;
-endmodule
