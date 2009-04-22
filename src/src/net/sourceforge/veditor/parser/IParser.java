59cef2fac345e6711ed1b6d36c595d573c1c9b7f
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-04-22 16:09:59 +0000

    Added AutoTask marker support
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@547 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/parser/IParser.java
+++ src/src/net/sourceforge/veditor/parser/IParser.java
@@ -15,6 +15,9 @@ public interface IParser
 	public static final int OUT_OF_MODULE = 0;
 	public static final int IN_MODULE = 1;
 	public static final int IN_STATEMENT = 2;
+	//These are tags used to set create tasks inside of comments. The first token is
+	//considered to have high priority. The rest are normal priorities
+	public static final String[] taskCommentTokens={"FIXME","TODO","FIXME:","TODO:"};
 
 	public void parse() throws HdlParserException;
 	public int getContext();
