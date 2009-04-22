5a96a36e12e70bc57b87a9cc92692de63ccb9b3e
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-04-22 16:05:06 +0000

    moved scanner classes to their own package under editor
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@541 3f283593-323e-0410-93fe-8b41104a9e7c

---
+++ src/src/net/sourceforge/veditor/editor/scanner/HdlPartitionScanner.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2006 KOBAYASHI Tadashi and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    KOBAYASHI Tadashi - initial API and implementation
+ *******************************************************************************/
+
+package net.sourceforge.veditor.editor.scanner;
+import net.sourceforge.veditor.editor.HdlTextAttribute;
+
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.RuleBasedPartitionScanner;
+import org.eclipse.jface.text.rules.Token;
+
+/**
+ * This is an abstract class that defines the common behavior
+ * between the VHDL and Verilog Partition scanners
+ */
+abstract public class HdlPartitionScanner extends RuleBasedPartitionScanner
+{
+	public static final String DOXYGEN_COMMENT = "__hdl_doxygen_comment";
+	public static final String SINGLE_LINE_COMMENT = "__hdl_singleline_comment";
+	public static final String MULTI_LINE_COMMENT = "__hdl_multiline_comment";
+	public static final String STRING = "__hdl_string";	
+	public static final String TASK_TAG = "_hdl_task_tag";
+	public static IToken stringToken = new Token(STRING);
+	public static IToken singleLineCommentToken = new Token(SINGLE_LINE_COMMENT);
+	public static IToken doxygenCommentToken = new Token(DOXYGEN_COMMENT);
+	public static IToken taskTagToken = new Token(TASK_TAG);
+	public static IToken multiLineCommentToken=new Token(MULTI_LINE_COMMENT);
+
+	
+	public static String[] getContentTypes()
+	{
+		return new String[] { 
+		        SINGLE_LINE_COMMENT,
+		        DOXYGEN_COMMENT, 
+		        MULTI_LINE_COMMENT,
+				STRING,
+				TASK_TAG
+				};
+	}
+
+	public static HdlTextAttribute[] getContentTypeAttributes()
+	{
+		// must be same sequence with getContentTypes
+		return new HdlTextAttribute[] { 
+		        HdlTextAttribute.SINGLE_LINE_COMMENT,
+		        HdlTextAttribute.DOXYGEN_COMMENT,
+				HdlTextAttribute.MULTI_LINE_COMMENT, 
+				HdlTextAttribute.STRING,
+				HdlTextAttribute.AUTOTASKS,
+				};
+	}
+
+	protected HdlPartitionScanner()
+	{
+		super();
+	}
+}
