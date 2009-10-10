59f5172ecabb1b116660da34fd53357390cc81c5
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-10-10 13:34:43 +0000

    From Stijin. Functionality to show matching parenthesis.
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@570 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/editor/VhdlEditor.java
+++ src/src/net/sourceforge/veditor/editor/VhdlEditor.java
@@ -16,18 +16,33 @@ import net.sourceforge.veditor.editor.scanner.HdlPartitionScanner;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewerExtension2;
+import org.eclipse.jface.text.source.DefaultCharacterPairMatcher;
+import org.eclipse.jface.text.source.ISourceViewerExtension2;
+import org.eclipse.jface.text.source.MatchingCharacterPainter;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 
 
 
 public class VhdlEditor extends HdlEditor
 {
 	static final String REPLACE_COMMENT="-- ";
-	
+	/** The editor's peer Parent Matcher */
+	  private DefaultCharacterPairMatcher fParentMatcher;
+	  private Color colorMatchingChar;
+	  
+	  /** The editor's peer character painter */
+	  private MatchingCharacterPainter fMatchingCharacterPainter;
+	  
 	public VhdlEditor()
 	{
 		super();
+		char[] chars = new char[] {'(', ')'};
+		fParentMatcher = new DefaultCharacterPairMatcher(chars);
 		setDocumentProvider(new VhdlDocumentProvider());
 		setSourceViewerConfiguration(HdlSourceViewerConfiguration
 				.createForVhdl(getColorManager()));
@@ -38,6 +53,9 @@ public class VhdlEditor extends HdlEditor
 	public void createPartControl(Composite parent)
 	{
 	    super.createPartControl(parent);
+	    
+	    showMatchingCharacters();
+	    
 	    if (getViewer() instanceof SourceViewer) {
 			SourceViewer viewer = (SourceViewer) getViewer();
 	
@@ -63,4 +81,29 @@ public class VhdlEditor extends HdlEditor
 		}
 		
 	}
+	  /**
+	   * Add a Painter to show matching characters.
+	   */
+	  private final void showMatchingCharacters() {
+	    if (fMatchingCharacterPainter == null) {
+	      if (getSourceViewer() instanceof ISourceViewerExtension2) {
+	        fMatchingCharacterPainter = new MatchingCharacterPainter(
+	            getSourceViewer(), fParentMatcher);
+	        Display display = Display.getCurrent();
+	        //IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+	        //colorMatchingChar = new Color(display, PreferenceConverter.getColor(
+	        //    store, JJPreferences.P_MATCHING_CHAR));
+	        colorMatchingChar = new Color(display,new RGB(128,128,128));
+	        fMatchingCharacterPainter.setColor(colorMatchingChar);
+	        ITextViewerExtension2 extension = (ITextViewerExtension2) getSourceViewer();
+	        extension.addPainter(fMatchingCharacterPainter);
+	      }
+	    }
+	  }
+	  
+
+		
+	
+	  
+
 }
