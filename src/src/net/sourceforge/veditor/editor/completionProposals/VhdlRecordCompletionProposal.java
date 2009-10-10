917edb7256c2eec6f2ea29b3ec1fc6b1cae9b4da
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-10-10 17:41:46 +0000

    From Stijin. Implementation of Record member auto completion (when pressing "." after record instance name)
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@579 3f283593-323e-0410-93fe-8b41104a9e7c

---
+++ src/src/net/sourceforge/veditor/editor/completionProposals/VhdlRecordCompletionProposal.java
@@ -0,0 +1,34 @@
+package net.sourceforge.veditor.editor.completionProposals;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+
+import net.sourceforge.veditor.VerilogPlugin;
+import net.sourceforge.veditor.editor.HdlCompletionProcessor;
+import net.sourceforge.veditor.editor.completionProposals.*;
+
+public class VhdlRecordCompletionProposal extends CompletionProposal {
+	private int cursor;
+	private String display;
+
+	public VhdlRecordCompletionProposal(String replace, int offset, int length,
+			int cursor, String display) {
+		super(replace, offset, length);
+		this.cursor = cursor;
+		this.display = display;
+	}
+
+	public String getDisplayString() {
+		return display;
+	}
+
+	public Point getSelection(IDocument document) {
+		return new Point(getOffset() - getLength() + cursor, 0);
+	}
+
+	public Image getImage() {
+		return VerilogPlugin.getPlugin().getImage("$nl$/icons/obj.gif");
+	}
+
+}
