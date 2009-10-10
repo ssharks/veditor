68d3952e9ae8d0a6c4699e23551ae72741c4a61f
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2009-10-10 17:52:05 +0000

    fixed warnings
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@580 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/editor/completionProposals/VhdlRecordCompletionProposal.java
+++ src/src/net/sourceforge/veditor/editor/completionProposals/VhdlRecordCompletionProposal.java
@@ -5,8 +5,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 
 import net.sourceforge.veditor.VerilogPlugin;
-import net.sourceforge.veditor.editor.HdlCompletionProcessor;
-import net.sourceforge.veditor.editor.completionProposals.*;
 
 public class VhdlRecordCompletionProposal extends CompletionProposal {
 	private int cursor;
