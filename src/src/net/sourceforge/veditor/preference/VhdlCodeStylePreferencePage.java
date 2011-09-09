65e91184284eadcf80d05d98fc181addaae1f75b
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2011-09-09 20:25:36 +0000

    Used common strings in PreferenceStrings instead of hard-coded values.
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@645 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/preference/VhdlCodeStylePreferencePage.java
+++ src/src/net/sourceforge/veditor/preference/VhdlCodeStylePreferencePage.java
@@ -15,21 +15,16 @@ import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.swt.widgets.Composite;
 
 public class VhdlCodeStylePreferencePage extends AbstractSimplePreferencePage {
-	public static final String PAD_OPERATORS="Style.Vhdl.PadOperators";
-	public static final String INDENT_LIBRARY="Style.Vhdl.IndentLibrary";
-	public static final String KEYWORDS_LOWERCASE="Style.Vhdl.KeywordsLowercase";
-	public static final String ALIGNONARROWRIGHT="Style.Vhdl.AlignOnArrowRight";
-	public static final String ALIGNONARROWLEFT="Style.Vhdl.AlignOnArrowLeft";
-	public static final String ALIGNONCOLON="Style.Vhdl.AlignOnColon";
+
 	@Override
 	protected void createFieldEditors() {
 		Composite parent = getFieldEditorParent();
-		addField(new BooleanFieldEditor(PAD_OPERATORS,"Pad operators",parent));
-		addField(new BooleanFieldEditor(INDENT_LIBRARY,"Indent Library use",parent));
-		addField(new BooleanFieldEditor(KEYWORDS_LOWERCASE,"keywords in lowercase",parent));
-		addField(new BooleanFieldEditor(ALIGNONARROWRIGHT,"Align On =>",parent));
-		addField(new BooleanFieldEditor(ALIGNONARROWLEFT,"Align On <=",parent));
-		addField(new BooleanFieldEditor(ALIGNONCOLON,"Align On :",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.PAD_OPERATORS,"Pad operators",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.INDENT_LIBRARY,"Indent Library use",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.KEYWORDS_LOWERCASE,"keywords in lowercase",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.ALIGNONARROWRIGHT,"Align On =>",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.ALIGNONARROWLEFT,"Align On <=",parent));
+		addField(new BooleanFieldEditor(PreferenceStrings.ALIGNONCOLON,"Align On :",parent));
 	}
 
 }
