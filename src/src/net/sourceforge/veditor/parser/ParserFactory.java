59f1e36287569c580b6b485b3ea73a33231342fd
aghoras <aghoras@3f283593-323e-0410-93fe-8b41104a9e7c>
2011-09-09 20:05:29 +0000

    Created a new reader for reading Strings and InputStreams. This new reader can be stopped in order to facilitate an orderly shutdown of parser threads when encountering large files.
    git-svn-id: http://svn.code.sf.net/p/veditor/code/trunk@633 3f283593-323e-0410-93fe-8b41104a9e7c

--- src/src/net/sourceforge/veditor/parser/ParserFactory.java
+++ src/src/net/sourceforge/veditor/parser/ParserFactory.java
@@ -10,10 +10,6 @@
  *******************************************************************************/
 package net.sourceforge.veditor.parser;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-
 import net.sourceforge.veditor.parser.verilog.VerilogParser;
 import net.sourceforge.veditor.parser.vhdl.VhdlParser;
 
@@ -29,76 +25,29 @@ abstract public class ParserFactory
 	private ParserFactory()
 	{
 	}
-	
-//	public static IParser create(Reader reader, IFile file)
-//	{
-//		String extension = file.getFileExtension();
-//		if (extension.equals("v"))
-//			return new VerilogParser(new AsciiReader(reader));
-//		else if (extension.equals("vhd") || extension.equals("VHD"))
-//			return new VhdlParser(new AsciiReader(reader));
-//		else
-//			return null;
-//	}
-	
+		
 	public static IParser createVerilogParser(String text, IProject project, IFile file)
-	{
-		return new VerilogParser(new AsciiReader(text), project, file);
+	{   
+	    ParserReader reader=new ParserReader(text);
+		return new VerilogParser(reader, project, file);
 	}
 
-	public static IParser createVerilogParser(Reader reader, IProject project, IFile file)
-	{
+	public static IParser createVerilogParser(ParserReader reader, IProject project, IFile file)
+	{	    
 		return new VerilogParser(reader, project, file);
 	}
 
 	public static IParser createVhdlParser(String text, IProject project, IFile file)
 	{
-		return new VhdlParser(new AsciiReader(text), project, file);
+	    ParserReader reader=new ParserReader(text);
+		return new VhdlParser(reader, project, file);
 	}
 
-	public static IParser createVhdlParser(Reader reader, IProject project, IFile file)
+	public static IParser createVhdlParser(ParserReader reader, IProject project, IFile file)
 	{
 		return new VhdlParser(reader, project, file);
 	}
 
-	/**
-	 *	Wrapper of StringReader. It can support rewind.
-	 *  @note
-	 *  VHDL parser cannot handle two byte character.
-	 *  So AsciiReader change two byte character to single space.
-	 */
-	private static class AsciiReader extends Reader
-	{
-		private String text;
-		private StringReader reference;
-
-		public AsciiReader(String text)
-		{
-			this.text = text;
-			reset();
-		}
-		
-		public void close() throws IOException
-		{
-			reference.close();
-		}
-
-		public int read(char[] cbuf, int off, int len) throws IOException
-		{
-			int n = reference.read(cbuf, off, len);
-			for(int i = 0; i < n; i++)
-			{
-				if (cbuf[i] >= 0x0100)
-					cbuf[i] = ' ';
-			}
-			return n;
-		}
-
-		public void reset()
-		{
-			reference = new StringReader(text);
-		}
-	}
 }
 
 
